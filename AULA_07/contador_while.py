88055ba18d1c2d63fdf0573774c06250ac2157f9
José William <prof.williamfirmino@gmail.com>
2025-09-26 11:38:13 -0300

    Alterações finais do dia 26/09/2025

---
+++ AULA_07/contador_while.py
@@ -0,0 +1,17 @@
+# Monte um sistema utilizando while onde uma mensagem será exibida para o usuário com o número atual até que o valor seja 0.
+# contador = 10
+# while contador > 0:
+#     print(f"O número atual é o {contador}")
+#     contador -= 1
+#     # contador = contador -1 # Decrementar
+# print("Contagem encerrada!")
+
+# Monte um sistema utilizando while onde uma mensagem será exibida para o usuário com o número atual até que o valor seja 0. Quando o valor for 6, não exiba a mensagem.
+contador = 10
+while contador > 0:
+    if contador == 6:
+        continue
+    print(f"O número atual é o {contador}")
+    contador -= 1
+    # contador = contador -1 # Decrementar
+print("Contagem encerrada!")
---
+++ AULA_07/pratica_while.py
@@ -0,0 +1,12 @@
+# Faça um programa em python utilizando while onde você irá perguntar ao usuário qual o sabor da pizza que ele quer. Continue perguntando até que o cliente digite sair. 
+#pedido = "" # Força explicitamente a variável pedido a ser do tipo string.
+
+while True: 
+    pedido = input(" Faça seu pedido (digite 'sair' para encerrar):") 
+    if pedido.lower() == "sair":
+        print(" ❌ Saindo do sistema ...")
+        break
+    print(f"🍕 {pedido} adicionado com sucesso.")
+
+print(" 🖖 Agradecemos a preferência. Volte sempre.")
+
