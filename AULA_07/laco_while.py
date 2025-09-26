802239bd0eed54948fc00c607f1b38c8fd0afb24
JOSE WILLIAM FIRMINO BEZERRA <jose.bezerra@educacao.senac.rio>
2025-09-26 10:03:44 -0300

    Alterações antes do intervalo - 26/09

---
+++ AULA_07/laco_while.py
@@ -0,0 +1,21 @@
+# 1ª Possibilidade - Com condição lógica
+# nome_usuario = input("Digite o seu nome: ")
+
+# while nome_usuario != "jose": # Enquanto o nome_do_usuario for diferente de jose.
+#     print(f"Seja bem vindo {nome_usuario}")
+#     nome_usuario = input("Digite o seu nome: ")
+
+# 2ª Possibilidade - Com condição lógica mas sem perguntar ao usuário a condição.
+# nome_usuario = True
+# while nome_usuario:
+#     nome_usuario = input("Digite o seu nome: ")
+#     print(f"Seja bem vindo {nome_usuario}")
+#     if nome_usuario == "jose":
+#         break # Quebra o laço de repetição mais próximo
+
+# 3ª Possibilidade - Sem precisar passar condição lógica
+while True:
+    nome_usuario = input("Digite o seu nome: ")
+    print(f"Seja bem vindo {nome_usuario}")
+    if nome_usuario == "jose":
+        break # Quebra o laço de repetição mais próximo
--- comentarios.md
+++ comentarios.md
@@ -7,3 +7,9 @@
 2. [else] -> Senão, é opcional e só pode usar uma vez.
 3. [elif] -> Senão se, é opcional e pode ser utilizado várias vezes.
 
+# LAÇOS
+1. [while] -> Executa um bloco de código enquanto uma condição for verdadeira.
+
+=> Utilizado quando você não sabe exatamente quantas vezes irá ocorrer a repetição.
+=> O while espera uma condição lógica ou True para começar a rodar.
+=> break, continue e in são funcionalidades que podem ser utilizadas no contexto do while.
